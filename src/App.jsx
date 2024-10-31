ea5f1ed8cb1175a3f5d1e891272addeaf12fd8e5
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-10-31 19:19:49 +0330

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,128 +4,29 @@ import { useState } from "react";
 // Update => state change
 //Unmount
 const App = () => {
-  // https://reactjsquiz.netlify.app/quiz.json
   const [isShowModal, setShowModal] = useState(false);
 
   const [step, setStep] = useState(0);
 
-  const [answer, setAnswer] = useState(false);
+  const [chouse, setChouse] = useState(false);
+
+  const [inCorrectOption, setInCorrectOption] = useState(null);
 
   const [score, setScore] = useState(0);
 
-  const [quiesData, setQuiesData] = useState([
-    {
-      id: "01",
-      question: "Which of the following is the correct name of React.js?",
-      options: ["React", "React.js", "ReactJS", "All of the above"],
-      answer: "All of the above",
-    },
-    {
-      id: "02",
-      question: "Which of the following are the advantages of React.js?",
-      options: [
-        "React.js can increase the application's performance with Virtual DOM.",
-        "React.js is easy to integrate with other frameworks such as Angular, BackboneJS since it is only a view library.",
-        "React.js can render both on client and server side.",
-        "All of the above",
-      ],
-      answer: "All of the above",
-    },
-    {
-      id: "03",
-      question: "Which of the following is not a disadvantage of React.js?",
-      options: [
-        "React.js has only a view layer. We have put your code for Ajax requests, events and so on.",
-        "The library of React.js is pretty large.",
-        "The JSX in React.js makes code easy to read and write.",
-        "The learning curve can be steep in React.js.",
-      ],
-      answer: "The JSX in React.js makes code easy to read and write.",
-    },
-    {
-      id: "04",
-      question:
-        "Which of the following command is used to install create-react-app?",
-      options: [
-        "npm install -g create-react-app",
-        "npx create-react-app my-app",
-        "npm install create-react-app",
-        "npm install -f create-react-app",
-      ],
-      answer: "npx create-react-app my-app",
-    },
-    {
-      id: "05",
-      question:
-        "What of the following is used in React.js to increase performance?",
-      options: [
-        "Original DOM",
-        "Virtual DOM",
-        "Both A and B.",
-        "None of the above.",
-      ],
-      answer: "Virtual DOM",
-    },
-    {
-      id: "06",
-      question: "What is the default port where webpack-server runs?",
-      options: ["3000", "8080", "3030", "6060"],
-      answer: "3000",
-    },
-    {
-      id: "07",
-      question:
-        "How many numbers of elements a valid react component can return?",
-      options: ["1", "2", "3", "Unlimited"],
-      answer: "1",
-    },
-    {
-      id: "08",
-      question:
-        "What is the declarative way to render a dynamic list of components based on values in an array?",
-      options: [
-        "Using the reduce array method",
-        "Using the <Each /> component",
-        "Using the Array.map() method",
-        "With a for/while loop",
-      ],
-      answer: "Using the Array.map() method",
-    },
-    {
-      id: "09",
-      question: "What is a state in React?",
-      options: [
-        "A permanent storage.",
-        "Internal storage of the component.",
-        "External storage of the component.",
-        "None of the above.",
-      ],
-      answer: "Internal storage of the component.",
-    },
-    {
-      id: "10",
-      question: "What are the two ways to handle data in React?",
-      options: [
-        "State & Props",
-        "Services & Components",
-        "State & Services",
-        "State & Component",
-      ],
-      answer: "State & Props",
-    },
-  ]);
+  const [quiesData, setQuiesData] = useState([]);
 
-  // const getQuizData = async () => {
-  //   const data = await fetch("https://reactjsquiz.netlify.app/quiz.json", {
-  //     method: "GET",
-  //   }).then((data) => data.json());
-  //   setQuiesData(data);
-  // };
+  const getQuizData = async () => {
+    const data = await fetch("https://reactjsquiz.netlify.app/quiz.json", {
+      method: "GET",
+    }).then((data) => data.json());
+    setQuiesData(data);
+  };
 
-  // useEffect(() => {
-  //   //Mount
-  //   getQuizData();
-  // }, []);
+  useEffect(() => {
+    //Mount
+    getQuizData();
+  }, []);
 
   const handleQuizApp = () => {
     setShowModal(true);
@@ -169,21 +70,22 @@ const App = () => {
 
               {quiesData[step].options.map((option, index) => (
                 <button
+                  key={index}
                   onClick={() => {
-                    if (option === quiesData[step].answer) {
+                    const item = quiesData[step];
+                    setChouse(true);
+
+                    if (item.answer !== option) {
+                      setInCorrectOption(option);
+                    } else {
                       setScore((prev) => prev + 5);
-                      setAnswer(true);
                     }
                   }}
-                  className={`p-2 text-left text-gray-50  rounded-sm ${
-                    !answer && "bg-gray-950"
-                  } ${
-                    answer &&
-                    option === quiesData[step].answer &&
-                    "bg-green-800"
-                  } ${
-                    answer && option !== quiesData[step].answer && "bg-red-800"
-                  }  `}
+                  className={`p-2 text-left text-gray-50  rounded-sm bg-gray-950  ${
+                    chouse &&
+                    quiesData[step].answer === option &&
+                    "bg-green-700"
+                  } ${chouse && option === inCorrectOption && "bg-red-800"} `}
                 >
                   {option}
                 </button>
@@ -191,8 +93,14 @@ const App = () => {
 
               <button
                 onClick={() => {
-                  setStep((prev) => prev + 1);
-                  setAnswer(false);
+                  if (step < 9) {
+                    setStep((prev) => prev + 1);
+                  }
+
+                  if (step === 8) {
+                    setShowModal(false);
+                  }
+                  setChouse(false);
                 }}
                 className="bg-indigo-700 py-2 text-gray-100 rounded-sm"
               >
@@ -202,6 +110,30 @@ const App = () => {
           </div>
         </div>
       ) : undefined}
+
+      {step === 9 && (
+        <div
+          className={`fixed top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 rounded-sm ${
+            score >= 25 ? "bg-green-600" : "bg-red-600"
+          } text-gray-100 flex gap-4 justify-center flex-col items-center py-10`}
+        >
+          <span className="font-bold text-3xl">{`${
+            score >= 25 ? "Awesome!" : "Ordinary!"
+          }`}</span>
+          <span>{`Your score is ${score} out of 50`}</span>
+
+          <button
+            onClick={() => {
+              setScore(0);
+              setStep(0);
+              setChouse(false);
+            }}
+            className="bg-green-100 text-gray-950 rounded-sm px-4 py-2"
+          >
+            Start Over
+          </button>
+        </div>
+      )}
     </>
   );
 };
